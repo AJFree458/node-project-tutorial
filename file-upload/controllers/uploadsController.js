7e84c28e223c8e31ace7d56a8f5ecdb901b3fb2c
AJFree458 <rthr.frmn@gmail.com>
2021-11-30 11:30:26 -0500

    Setup file upload functionality

---
+++ file-upload/controllers/uploadsController.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
+const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No File Uploaded");
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload Image");
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("Please Upload a Smaller Image");
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+module.exports = {
+  uploadProductImage,
+};
