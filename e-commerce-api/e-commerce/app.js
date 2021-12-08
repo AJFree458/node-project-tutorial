eb76969594cfe2f148f5cd5588311b135fcab67c
AJFree458 <rthr.frmn@gmail.com>
2021-12-07 19:25:23 -0500

    Complete cookie and jwt

--- e-commerce-api/e-commerce/app.js
+++ e-commerce-api/e-commerce/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 // rest of packages
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 // database
 const connectDB = require("./db/connect");
@@ -22,8 +23,14 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 // routes
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get("/", (req, res) => {
+  res.send("e-commerce api");
+});
+
+app.get("/api/v1", (req, res) => {
+  console.log(req.signedCookies);
   res.send("e-commerce api");
 });
 
