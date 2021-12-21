b664d24182f0d67a562a192bf68da8dc71d832b6
AJFree458 <rthr.frmn@gmail.com>
2021-12-20 22:54:34 -0500

    Product material added

--- e-commerce-api/e-commerce/app.js
+++ e-commerce-api/e-commerce/app.js
@@ -9,6 +9,7 @@ const app = express();
 // rest of packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 // database
 const connectDB = require("./db/connect");
@@ -16,6 +17,7 @@ const connectDB = require("./db/connect");
 // routers
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -25,6 +27,9 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static("./public/"));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.send("e-commerce api");
@@ -37,6 +42,7 @@ app.get("/api/v1", (req, res) => {
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
