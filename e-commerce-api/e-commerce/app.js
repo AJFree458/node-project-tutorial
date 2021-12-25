2bd33ec5bb2009e668ac7c64fe66a8e53405531f
AJFree458 <rthr.frmn@gmail.com>
2021-12-24 21:59:59 -0500

    Finish project

--- e-commerce-api/e-commerce/app.js
+++ e-commerce-api/e-commerce/app.js
@@ -10,6 +10,11 @@ const app = express();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 // database
 const connectDB = require("./db/connect");
@@ -25,22 +30,35 @@ const orderRouter = require("./routes/orderRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 60,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 // routes
-app.use(morgan("tiny"));
+// app.use(morgan("tiny")); not needed for non dev
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static("./public/"));
 app.use(fileUpload());
 
-app.get("/", (req, res) => {
+// can have these commented out for now
+/* app.get("/", (req, res) => {
   res.send("e-commerce api");
 });
 
 app.get("/api/v1", (req, res) => {
   console.log(req.signedCookies);
   res.send("e-commerce api");
-});
+}); */
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
--- e-commerce-api/e-commerce/controllers/orderController.js
+++ e-commerce-api/e-commerce/controllers/orderController.js
@@ -1,26 +1,124 @@
 const Order = require("../models/Order");
+const Product = require("../models/Product");
+
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const { checkPermissions } = require("../utils");
 
-const createOrder = (req, res) => {
-  res.send("create order");
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = "someRandomValue";
+  return { client_secret, amount };
 };
 
-const getAllOrders = (req, res) => {
-  res.send("get all orders");
+const createOrder = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError("No cart items provided");
+  }
+
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      "Please provide tax and shipping fee"
+    );
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(
+        `No product with id: ${item.product}`
+      );
+    }
+
+    const { name, price, image, _id } = dbProduct;
+    console.log(name, price, image);
+
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+
+    // Add item to order
+    orderItems = [...orderItems, singleOrderItem];
+
+    // Calculate subtotal
+    subtotal += item.amount * price;
+  }
+
+  // Calculate total
+  const total = tax + shippingFee + subtotal;
+
+  // get client secret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: "usd",
+  });
+
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
 };
 
-const getSingleOrder = (req, res) => {
-  res.send("get single order");
+const getAllOrders = async (req, res) => {
+  const orders = await Order.find({});
+  res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };
 
-const getCurrentUserOrders = (req, res) => {
-  res.send("get current user orders");
+const getSingleOrder = async (req, res) => {
+  const { id: orderId } = req.params;
+
+  const order = await Order.findOne({ _id: orderId });
+
+  if (!order) {
+    throw new CustomError.NotFoundError(`No order with id: ${orderId}`);
+  }
+
+  checkPermissions(req.user, order.user);
+
+  res.status(StatusCodes.OK).json({ order });
 };
 
-const updateOrder = (req, res) => {
-  res.send("update order");
+const getCurrentUserOrders = async (req, res) => {
+  const orders = await Order.findOne({ user: req.user.userId });
+
+  res.status(StatusCodes.OK).json({ orders, count: orders.length });
+};
+
+const updateOrder = async (req, res) => {
+  const { id: orderId } = req.params;
+
+  const { paymentIntentId } = req.body;
+
+  const order = await Order.findOne({ _id: orderId });
+
+  if (!order) {
+    throw new CustomError.NotFoundError(`No order with id: ${orderId}`);
+  }
+
+  checkPermissions(req.user, order.user);
+
+  order.paymentIntentId = paymentIntentId;
+  order.status = "paid";
+  await order.save();
+
+  res.status(StatusCodes.OK).json({ order });
 };
 
 module.exports = {
